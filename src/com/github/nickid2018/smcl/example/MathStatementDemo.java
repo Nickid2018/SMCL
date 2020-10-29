349e23d1c4a0edfb625c5aaabe622add5d4cbd63
Nickid2018 <2833231379@qq.com>
2020-10-29 15:17:03 +0800

    Ehhh..

--- src/com/github/nickid2018/smcl/example/MathStatementDemo.java
+++ src/com/github/nickid2018/smcl/example/MathStatementDemo.java
@@ -6,15 +6,8 @@ import com.github.nickid2018.smcl.parser.*;
 public class MathStatementDemo {
 
 	public static void main(String[] args) throws Exception {
-        SMCL smcl = SMCL.getInstance();
-        smcl.init();
         long t = System.currentTimeMillis();
-        Statement statement = BaseParser.parseStatement("2", smcl);
-        System.out.println(System.currentTimeMillis() - t);
-        t = System.currentTimeMillis();
-        System.out.println(statement.calculate(new VariableList()));
-        System.out.println(System.currentTimeMillis() - t);
-        t = System.currentTimeMillis();
+        System.out.println(RPNParser.calcFourOpreationResult(RPNParser.makeOnlyFourOperationRPN("4*1+(2.9/0.1)*4/2")));
         System.out.println(System.currentTimeMillis() - t);
     }
 }
